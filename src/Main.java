fe45f4471892b2ad7974349e6e8be36183120b84
kappagsy <kappagsy@mail.uc.edu>
2022-09-29 13:25:33 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        double celsiusTemp = 0;
+        double fahrenheitTemp = 0;
+
+
+        System.out.print("What is the temperature in Celsius? ");
+        if (in.hasNextDouble())
+        {
+            celsiusTemp = in.nextDouble();
+            fahrenheitTemp = (celsiusTemp * 9/5) + 32;
+            in.nextLine();
+            System.out.println("This temperature in Fahrenheit is: " + fahrenheitTemp + " degrees.");
+        }
+        else
+        {
+            System.out.print("You entered an invalid input.");
+        }
+    }
+}
